df5584b43fd9f23b3ab275d49c54b4feebdf8cc0
Morty67 <116444521+Morty67@users.noreply.github.com>
2022-10-31 23:04:35 +0200

    Update Homework2Lecture.py

--- Homework2Lecture.py
+++ Homework2Lecture.py
@@ -3,19 +3,18 @@
 print(int(input('Enter your digits')) < 0)
 
         #2. Write a Python program to check if the value a is less than 20 or not
-a = int(input('Enter digit - A: '))
-b = 20
-print(not a >= b)
+
+print(not (a := int(input('Enter digit - A: '))) >= (b := 20))
 
 
         # 3. Write a Python program to check if a given number is Zero or Not.
-n = int(input('Enter your digit: '))
-print(not n)
+
+print(not (n := int(input('Enter your digit: '))))
 
 
         #4. Write a Python program to check if a given number is Even or Odd.
-digit = int(input('Enter your digit: '))
-print('Even' if digit % 2 == 0 else 'Odd')
+
+print(not (digit := int(input('Enter your digit: '))) % 2 and 'Even' or 'Odd')
 
         #5. Write a Python program to find largest number among three numbers
         #entered by user.
