721799be512ee3b6866af6c7a268dc6614ea1459
Morty67 <116444521+Morty67@users.noreply.github.com>
2023-01-18 14:06:11 +0200

    Update HomeWork16.py

--- HomeWork16.py
+++ HomeWork16.py
@@ -1,11 +1,11 @@
 
 class Product:
     def __init__(self, description, price, size):
+        if self.price <= 0:
+            raise ValueError('Prise must be only positive')
         self.description = description
         self.price = price
         self.size = size
-        if self.price <= 0:
-            raise ValueError('Prise must be only positive')
 
     def __str__(self):
         return f'Product - {self.description}, size - {self.size}, price - {self.price}'
