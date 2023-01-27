769ba0483beebc4a04058a576290dc400b6a1857
Morty67 <116444521+Morty67@users.noreply.github.com>
2023-01-27 19:53:42 +0200

    Update HomeWokr7.py

--- HomeWokr7.py
+++ HomeWokr7.py
@@ -1,4 +1,5 @@
-#Task1
+#Task1 Using a dictionary, write a program that displays the name of the day of the week by number.
+# For example, 1 is "Monday".
 weeks = {
          1: 'Monday',
          2: 'Tuesday',
@@ -9,8 +10,8 @@ weeks = {
          7: 'Sunday',
          }
 print(weeks.get((x := int(input('What day you need:? '))), 'Error, only 1-7'))
-
-#Task2
+ 
+#Task2 Describe a cat (pet) using a dictionary.
 
 my_cat = {
     'name': 'Snuffles',
@@ -24,15 +25,17 @@ my_cat = {
     }
 }
 
-#Task3
+#Task3  Write a program that reads a string of text from the keyboard and displays statistics on how many times each letter appears in this string.
+# For example, for the string "Hello world" this statistics looks like this: "H" - 1, "e" - 1, "l" - 3, etc.
 
 text = 'Hello, World'
 print(res := {i: text.count(i) for i in text if i.isalpha()})
 
-#Task4
+#Task4 Write a program that reads two lines of text from the keyboard and displays the letters that appear simultaneously in the first and second lines. 
+# For example, for the strings "Hello" and "World", the letters "l" and "o" should appear on the screen.
 print(set(s := (input('Enter your string: '))) & (set(s1 := input('Enter your 2 string: '))))
 
-#Task5
+#Task5 Write a program that will generate two lists. One with numbers in multiples of 3, the other with numbers in multiples of 5.
 
 list_for3 = []
 list_for5 = []
@@ -42,5 +45,5 @@ for i in range(3, 50):
     if not i % 5:
         list_for5.append(i)
 print(list_for3, list_for5)
-# #Task6
+# #Task6 Create a list with numbers that appear in both lists.
 print(res := set(list_for3) & set(list_for5))
