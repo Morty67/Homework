45e2ff4b47834d74a30d8af21f214498344c00d8
Morty67 <116444521+Morty67@users.noreply.github.com>
2023-01-27 12:02:14 +0200

    Update HomeWork18.py

--- HomeWork18.py
+++ HomeWork18.py
@@ -1,4 +1,6 @@
-# Task1
+# Task1 Implement a generator function that will return one member of a numerical sequence, the law of which is specified using a user function.
+# In addition, the parameters of the generator function must be the value of the first term of the progression and the number of terms produced by the sequence.
+# The generator must stop its work either when the nth term is reached or when the command to end is sent.
 
 def my_gen_func(num: int = 1, count: int = 5, func_from_user: object = None):
 
@@ -18,7 +20,9 @@ for item in my_gen_func(1, 15, func_from_user):
 
 
 
-# Task2
+# Task2 Using closures, implement such a programming technique as Memoization
+# Use the resulting mechanism to speed up the function of recursively calculating the nth term of the Fibonacci series.
+# Compare the speed of execution with a simple recursive approach.
 
 from timeit import timeit
 
@@ -47,7 +51,7 @@ print(f'Time for recursion - {timeit(temp1_rec, number=20)}')
 print(f'Time for memoization - {timeit(temp2_mem, number=20)}')
 
 
-# Task3
+# Task3 Write a function that applies an arbitrary user-defined function to a list of numbers and returns the sum of the elements of the resulting list.
 
 def seq(my_list: list, user_func):
     res = (item for item in my_list if user_func(item))
